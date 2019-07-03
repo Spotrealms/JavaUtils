d08415c319bbca84612bad750a2974cfa974a016
Spotlightsrule <19472287+Spotlightsrule@users.noreply.github.com>
2019-07-03 12:38:07 -0700

    Add greater time precision options to the NanoBenchmark class

--- src/main/java/com/spotrealms/javautils/terminal/NanoBenchmark.java
+++ src/main/java/com/spotrealms/javautils/terminal/NanoBenchmark.java
@@ -18,33 +18,102 @@
 
 package com.spotrealms.javautils.terminal;
 
+//Import Java classes and dependencies
+import java.math.BigDecimal;
+
 //TODO: Add JavaDoc
 
+//Adapted from kangcz's TimperImpl class (https://stackoverflow.com/a/44036528/7520602)
 public class NanoBenchmark {
-	private long starterMillis;
-	private long elapsedMillis;
+	private long starterTime;
+	private long elapsedTime;
+	private long nanoOffset;
+	private measPrecision benchPrecision;
 
 	//Class constructor
-	public NanoBenchmark(){}
+	public NanoBenchmark(measPrecision benchPrecision){
+		//Assign the class variables from the constructor's parameters
+		this.benchPrecision = benchPrecision;
+		
+		//Create a counter for the for loop (to get the nanosecond offset)
+		final int loopCount = 500;
+		
+		//Zero the nanosecond offset (more precision)
+		BigDecimal offsetSum = BigDecimal.ZERO;
+		
+		for(int i=0; i < loopCount; i++){
+			//Calculate the nanosecond offset
+			offsetSum = offsetSum.add(BigDecimal.valueOf(calculateNanoOffset()));
+		}
+		
+		//Get the nanosecond offset by dividing the offset sum by the loop count
+		nanoOffset = (offsetSum.divide(BigDecimal.valueOf(loopCount))).longValue();
+	}
+	
+	public NanoBenchmark(){
+		//Redirect to the overloaded constructor with unimplemented parameters being substituted by their default values
+		this(measPrecision.MILLISEC);
+	}
+	
+	//Define precision levels
+	enum measPrecision {
+		NANOSEC, MICROSEC, MILLISEC, SEC;
+	}
 	
 	public void start(){
-		//Get the current millis and begin the process of benchmarking
-		starterMillis = System.currentTimeMillis();
+		//Get the current system timestamp using the precision given upon instantiation
+		starterTime = getCurrentTimestamp();
 	}
 	
 	public void stop(){
-		//Calculate the elapsed amount of time since the start of the benchmark (current millis - initial millis)
-		elapsedMillis = (System.currentTimeMillis() - starterMillis);
+		//Calculate the elapsed amount of time since the start of the benchmark (current timestamp - initial timestamp)
+		elapsedTime = (getCurrentTimestamp() - starterTime);
+	}
+	
+	private static long calculateNanoOffset(){
+		//Get the current timestamp in nanoseconds
+		final long nanoTime = System.nanoTime();
+		
+		//Attempt to get the equivalent time in milliseconds (less accurate)
+		final long nanoFromMilli = System.currentTimeMillis() * 1_000_000;
+		
+		//Calculate the difference between the two measurements
+		return (nanoFromMilli - nanoTime);
+	}
+	
+	private long getCurrentTimestamp(){
+		//Check the precision level
+		switch(benchPrecision){
+			case NANOSEC:
+				//Return the current system timestamp, in nanoseconds, with offset
+				return (nanoOffset + System.nanoTime());
+			
+			case MICROSEC:
+				//Return the current system timestamp, in microseconds, with offset
+				return ((nanoOffset + System.nanoTime()) / 1000);
+			
+			case MILLISEC:
+				//Return the current system timestamp, in milliseconds
+				return System.currentTimeMillis();
+				
+			case SEC:
+				//Return the current system timestamp, in seconds
+				return (System.currentTimeMillis() / 1000);
+				
+			default:
+				//Return the current system timestamp, in milliseconds
+				return System.currentTimeMillis();
+		}
 	}
 
 	//Getters
-	public long getStarterMillis(){
+	public long getStarterTime(){
 		//Return the time the benchmark started at
-		return starterMillis;
+		return starterTime;
 	}
 
-	public long getElapsedMillis(){
+	public long getElapsedTime(){
 		//Return the time it took for the benchmark to complete
-		return elapsedMillis;
+		return elapsedTime;
 	}
 }
